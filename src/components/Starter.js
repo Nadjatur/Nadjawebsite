b9584af68a7270a97f4c7fa527f95e7fd3becf9d
Nadja Tur <nadjatur1999@gmail.com>
2023-05-02 15:55:57 +0200

    first

---
+++ src/components/Starter.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./../scss/Starter.scss";
+
+function Starter() {
+    return (
+        <div>
+          <h1>hi</h1>  
+        </div>
+    )
+}
+
+export default Starter;
