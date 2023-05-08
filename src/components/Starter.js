3e1d5c902d5ab215eef8fad22fda335266af770a
Nadja Tur <nadjatur1999@gmail.com>
2023-05-08 15:39:43 +0200

    hero added

--- src/components/Starter.js
+++ src/components/Starter.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import "./../scss/Starter.scss";
+import myVideo from '../assets/fairy.mp4';
 
 function Starter() {
     return (
-        <div>
-          <h1>hi</h1>  
+        <div className='bg'>
+            <video src={myVideo} autoPlay loop muted />
+            <h1>Nadja Tur website</h1>
         </div>
     )
 }
